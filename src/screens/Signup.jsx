b2fbee8eb6af6927d98c93e35c51083a3787bf10
Wasiullah Khalique <79196849+Wasi-25@users.noreply.github.com>
2023-05-12 18:39:54 +0530

    Update Signup.jsx

--- src/screens/Signup.jsx
+++ src/screens/Signup.jsx
@@ -9,7 +9,7 @@ const Signup = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("http://localhost:8000/api/createuser", {
+        const response = await fetch("https://foodhut.onrender.com/api/createuser", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
