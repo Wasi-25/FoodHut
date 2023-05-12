22ef2e926bdfe8afc3cc2e26ab3ff769637d13a9
Wasiullah Khalique <79196849+Wasi-25@users.noreply.github.com>
2023-05-12 18:39:21 +0530

    Update Login.jsx

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
       e.preventDefault();
-      const response = await fetch("http://localhost:8000/api/login", {
+      const response = await fetch("https://foodhut.onrender.com/api/login", {
           method: "POST",
           headers: {
               "Content-Type": "application/json"
